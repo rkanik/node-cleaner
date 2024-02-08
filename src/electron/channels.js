329007125814bb5f509765c5275448ed9a158cce
RK Anik <30260735+rkanik@users.noreply.github.com>
2024-02-09 00:28:59 +0600

    Added includes excludes

--- src/electron/channels.js
+++ src/electron/channels.js
@@ -1,118 +1,136 @@
-import { shell, ipcMain } from 'electron'
-import { exec } from 'child_process'
-import { searchDir } from './fs'
-import fs from 'fs/promises'
-import path from 'path'
+import { shell, ipcMain } from "electron";
+import { exec } from "child_process";
+import { searchDir } from "./fs";
+import fs from "fs/promises";
+import path from "path";
 // import rimraf from/
 
 const {
-	TEST,
-	EACH_DIR,
-	OPEN_IN_EXPLORER,
-	FIND_NODE_MODULES,
-	OPEN_WITH_VS_CODE,
-	DELETE_NODE_MODULES,
-	PROGRESS_NODE_MODULES,
-	CANCELLED_SEARCHING_NODE_MODULES,
-	PROGRESS_DELETE_NODE_MODULES,
-	GET_SUB_DIRS
-} = require('../consts')._COMMANDS
+  TEST,
+  EACH_DIR,
+  OPEN_IN_EXPLORER,
+  FIND_NODE_MODULES,
+  OPEN_WITH_VS_CODE,
+  DELETE_NODE_MODULES,
+  PROGRESS_NODE_MODULES,
+  CANCELLED_SEARCHING_NODE_MODULES,
+  PROGRESS_DELETE_NODE_MODULES,
+  GET_SUB_DIRS,
+} = require("../consts")._COMMANDS;
 
 const isCancelled = {
-	searchingNodeModules: false
-}
+  searchingNodeModules: false,
+};
 
-export const registerChannels = win => {
+export const registerChannels = (win) => {
+  const send = (event, data) => {
+    win.webContents.send(event, data);
+  };
 
-	const send = (event, data) => {
-		win.webContents.send(event, data);
-	}
+  ipcMain.on(TEST, async (_, payload) => {
+    console.log(TEST, typeof payload, payload);
 
-	ipcMain.on(TEST, async (_, payload) => {
-		console.log(TEST, payload)
+    const folder = "C:";
 
-		const folder = 'C:'
+    const readdirRes = await fs
+      .readdir(folder, { withFileTypes: true })
+      .catch((err) => console.log("ERROR::", err.message));
 
-		const readdirRes = await fs
-			.readdir(folder, { withFileTypes: true })
-			.catch((err) => console.log('ERROR::', err.message))
+    const files = readdirRes
+      .filter((dirent) => dirent.isDirectory())
+      .map((dirent) => {
+        return {
+          name: dirent.name,
+          path: path.resolve(folder, dirent.name),
+        };
+      });
 
-		const files = readdirRes
-			.filter(dirent => dirent.isDirectory())
-			.map(dirent => {
-				return {
-					name: dirent.name,
-					path: path.resolve(folder, dirent.name)
-				}
-			})
+    send(TEST, files);
+  });
 
-		send(TEST, files);
-	})
+  ipcMain.on(GET_SUB_DIRS, async (_, payload) => {
+    const readdirRes = await fs
+      .readdir(payload.path, { withFileTypes: true })
+      .catch((err) => console.log("ERROR::", err.message));
 
-	ipcMain.on(GET_SUB_DIRS, async (_, payload) => {
+    const files = readdirRes
+      .filter((dirent) => dirent.isDirectory())
+      .map((dirent) => {
+        return {
+          name: dirent.name,
+          path: path.resolve(payload.path, dirent.name),
+        };
+      });
 
-		const readdirRes = await fs
-			.readdir(payload.path, { withFileTypes: true })
-			.catch((err) => console.log('ERROR::', err.message))
+    send(GET_SUB_DIRS, files);
+  });
 
-		const files = readdirRes
-			.filter(dirent => dirent.isDirectory())
-			.map(dirent => {
-				return {
-					name: dirent.name,
-					path: path.resolve(payload.path, dirent.name)
-				}
-			})
+  ipcMain.on(FIND_NODE_MODULES, async (_, { dir, includes, excludes }) => {
+    const nodeModules = await searchDir(dir.path, includes, {
+      excludes,
+      onEachDir(dir) {
+        send(EACH_DIR, dir);
+        return isCancelled.searchingNodeModules;
+      },
+      onFound(file) {
+        send(PROGRESS_NODE_MODULES, file);
+      },
+      onFormat(dir, file) {
+        const location = dir.split("\\");
+        return {
+          isSelected: false,
+          appName: [...location][location.length - 2],
+          appPath: dir.substr(0, dir.length - 1),
+          nodeModulesPath: dir + file,
+        };
+      },
+    });
+    isCancelled.searchingNodeModules = false;
+    send(FIND_NODE_MODULES, nodeModules);
+  });
 
-		send(GET_SUB_DIRS, files);
-	})
+  ipcMain.on(CANCELLED_SEARCHING_NODE_MODULES, () => {
+    isCancelled.searchingNodeModules = true;
+  });
 
-	ipcMain.on(FIND_NODE_MODULES, async (_, dir) => {
-		const nodeModules = await searchDir(dir.path, ['node_modules'], {
-			excludes: [
-				'.git', 'protanopia', 'Android Studio', 'vendor',
-				'allinone-dark', 'utilty.helpers', 'rkenger', 'rkanik-me',
-				'_Encoder', 'node-cleaner', 'golang'
-			],
-			onEachDir(dir) {
-				send(EACH_DIR, dir)
-				return isCancelled.searchingNodeModules
-			},
-			onFound(file) { send(PROGRESS_NODE_MODULES, file); },
-			onFormat(dir, file) {
-				const location = dir.split('\\')
-				return {
-					isSelected: false,
-					appName: [...location][location.length - 2],
-					appPath: dir.substr(0, dir.length - 1),
-					nodeModulesPath: dir + file
-				}
-			},
-		})
-		isCancelled.searchingNodeModules = false
-		send(FIND_NODE_MODULES, nodeModules);
-	})
+  ipcMain.on(OPEN_IN_EXPLORER, (_, dir) => {
+    console.log("OPEN_IN_EXPLORER", dir);
+    shell.showItemInFolder(dir);
+  });
 
-	ipcMain.on(CANCELLED_SEARCHING_NODE_MODULES, () => {
-		isCancelled.searchingNodeModules = true
-	})
+  ipcMain.on(OPEN_WITH_VS_CODE, (_, dir) => {
+    console.log("OPEN_WITH_VS_CODE", dir);
+    exec(`code ${dir}`);
+  });
 
-	ipcMain.on(OPEN_IN_EXPLORER, (_, dir) => {
-		console.log('OPEN_IN_EXPLORER', dir)
-		shell.showItemInFolder(dir)
-	})
+  ipcMain.on(DELETE_NODE_MODULES, (_, dirs) => {
+    const remover = (dir) => {
+      console.log(`rm -rf "${dir}"`);
+      exec(`rm -rf "${dir}"`, (err) => {
+        console.log("success", !err);
+        dirs.length && remover(dirs.shift());
+        !err && send(PROGRESS_DELETE_NODE_MODULES, dir);
+      });
+    };
 
-	ipcMain.on(OPEN_WITH_VS_CODE, (_, dir) => {
-		console.log('OPEN_WITH_VS_CODE', dir)
-		exec(`code ${dir}`)
-	})
-
-	ipcMain.on(DELETE_NODE_MODULES, (_, dirs) => {
-		for (let dir of dirs) {
-			exec(`rm -rf ${dir}`, (err) => {
-				!err && send(PROGRESS_DELETE_NODE_MODULES, dir)
-			})
-		}
-		send(DELETE_NODE_MODULES, dirs)
-	})
-}
+    remover(dirs.shift());
+    // for (let dir of dirs) {
+    //   // console.log(`rm -rf ${dir}`);
+    //   // exec(`rm -rf ${dir}`, (err, a, b) => {
+    //   //   console.log("callback", err, a, b);
+    //   //   !err && send(PROGRESS_DELETE_NODE_MODULES, dir);
+    //   // });
+    // }
+    // for (let dir of dirs) {
+    //   console.log(`rm -rf ${dir}`);
+    //   try {
+    //     execSync(`rm -rf ${dir}`);
+    //     console.log("Done");
+    //     send(PROGRESS_DELETE_NODE_MODULES, dir);
+    //   } catch (error) {
+    //     console.log("failed");
+    //   }
+    // }
+    send(DELETE_NODE_MODULES, dirs);
+  });
+};
--- src/views/NodeModules.vue
+++ src/views/NodeModules.vue
@@ -1,336 +1,331 @@
 <template>
-	<v-container>
-		<v-toolbar
-			flat
-			color="transparent"
-		>
-			<v-toolbar-title>Node Modules</v-toolbar-title>
-			<v-spacer></v-spacer>
-			<v-btn
-				small
-				depressed
-				class="mr-3"
-				color="success"
-				@click="sendTest"
-			>TEST</v-btn>
-			<v-btn
-				small
-				depressed
-				class="mr-3"
-				color="error"
-				v-if="loading"
-				@click="handleCancelSearchNodeModules"
-			>Cancel</v-btn>
-			<v-btn
-				small
-				depressed
-				color="primary"
-				:loading="loading"
-				@click="handleSearchNodeModules"
-			>Search Node Modules</v-btn>
-		</v-toolbar>
+  <v-container>
+    <v-toolbar flat color="transparent">
+      <v-toolbar-title>Node Modules</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn small depressed class="mr-3" color="success" @click="sendTest"
+        >TEST</v-btn
+      >
+      <v-btn
+        small
+        depressed
+        class="mr-3"
+        color="error"
+        v-if="loading"
+        @click="handleCancelSearchNodeModules"
+        >Cancel</v-btn
+      >
+      <v-btn
+        small
+        depressed
+        color="primary"
+        :loading="loading"
+        @click="handleSearchNodeModules"
+        >Search Node Modules</v-btn
+      >
+    </v-toolbar>
 
-		<v-combobox
-			v-model="currentDirModel"
-			item-text="path"
-			item-value="path"
-			:items="currentDirSubDirs"
-			placeholder="C:"
-			class="mx-4"
-			dense
-		>
+    <v-combobox
+      v-model="currentDirModel"
+      item-text="path"
+      item-value="path"
+      :items="currentDirSubDirs"
+      placeholder="C:"
+      class="mx-4"
+      dense
+      label="Search..."
+    >
+      <template #item="{item, on, attrs}">
+        <v-list-item v-on="on" v-bind="attrs" two-line dense>
+          <v-list-item-avatar>
+            <v-icon dark class="grey lighten-1">
+              mdi-folder
+            </v-icon>
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.name }}</v-list-item-title>
+            <v-list-item-subtitle>{{ item.path }}</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-combobox>
 
-			<template #item="{item, on, attrs}">
-				<v-list-item
-					v-on="on"
-					v-bind="attrs"
-					two-line
-					dense
-				>
-					<v-list-item-avatar>
-						<v-icon
-							dark
-							class="grey lighten-1"
-						>
-							mdi-folder
-						</v-icon>
-					</v-list-item-avatar>
-					<v-list-item-content>
-						<v-list-item-title>{{item.name}}</v-list-item-title>
-						<v-list-item-subtitle>{{item.path}}</v-list-item-subtitle>
-					</v-list-item-content>
-				</v-list-item>
-			</template>
-		</v-combobox>
+    <v-combobox
+      v-model="includes"
+      dense
+      multiple
+      placeholder="C:"
+      class="mx-4 mt-4"
+      label="Includes"
+    />
 
-		<div
-			class="px-4 caption truncate"
-			v-if="currentSearchDir"
-		>
-			<v-progress-circular
-				indeterminate
-				color="primary"
-				size="12"
-				width="1"
-			/>
-			{{ currentSearchDir }}
-		</div>
-		<v-toolbar
-			flat
-			dense
-			class="mx-4"
-			v-if="selectedNodeModules.length"
-		>
-			<v-btn
-				small
-				depressed
-				color="error"
-				@click="handleBulkDeleteNodeModules"
-			>
-				<v-icon left>mdi-delete</v-icon>
-				Delete selected
-			</v-btn>
-		</v-toolbar>
-		<v-list
-			dense
-			class="px-4"
-			color="transparent"
-		>
-			<v-list-item
-				two-line
-				:key="dirIndex"
-				:class="[dir.isSelected ? 'primary lighten-4' : 'white', {'mt-1': dirIndex}]"
-				v-for="(dir, dirIndex) in nodeModules"
-			>
-				<v-list-item-avatar
-					class="grey lighten-2"
-					@click="dir.isSelected = !dir.isSelected"
-				>
-					<v-checkbox
-						v-if="dir.isSelected"
-						readonly
-						hide-details
-						class="mt-0 pt-0"
-						style="width:24px"
-						v-model="checkboxIsChecked"
-					/>
-					<v-icon
-						v-else
-						dark
-						color="primary"
-					>mdi-folder</v-icon>
-				</v-list-item-avatar>
-				<v-list-item-content>
-					<v-list-item-title>{{dir.appName}}</v-list-item-title>
-					<v-list-item-subtitle>{{dir.appPath}}</v-list-item-subtitle>
-				</v-list-item-content>
-				<v-list-item-action>
-					<v-menu
-						transition="slide-x-transition"
-						bottom
-						right
-					>
-						<template v-slot:activator="{ on, attrs }">
-							<v-btn
-								icon
-								v-bind="attrs"
-								v-on="on"
-							>
-								<v-icon
-									size="20"
-									color="grey"
-									class="text--darken-1"
-								>mdi-dots-vertical</v-icon>
-							</v-btn>
-						</template>
-						<v-list dense>
-							<!-- OPEN IN FILE EXPLORER -->
-							<v-list-item
-								dense
-								@click="handleOpenInExplorer(dir.nodeModulesPath)"
-							>
-								<v-list-item-icon class="mr-3">
-									<v-icon
-										size="22"
-										color="orange"
-									>mdi-folder</v-icon>
-								</v-list-item-icon>
-								<v-list-item-content>
-									<v-list-item-title>Open in Explorer</v-list-item-title>
-								</v-list-item-content>
-							</v-list-item>
-							<!-- OPEN WITH VS CODE -->
-							<v-list-item
-								dense
-								@click="handleOpenInVSCode(dir.appPath)"
-							>
-								<v-list-item-icon class="mr-3">
-									<v-icon
-										size="22"
-										color="blue"
-									>mdi-microsoft-visual-studio-code</v-icon>
-								</v-list-item-icon>
-								<v-list-item-content>
-									<v-list-item-title>Open with Code</v-list-item-title>
-								</v-list-item-content>
-							</v-list-item>
-							<!-- ACTIONS -->
-							<v-divider></v-divider>
-							<v-list-item
-								dense
-								@click="handleDeleteNodeModules([dir.nodeModulesPath])"
-							>
-								<v-list-item-icon class="mr-3">
-									<v-icon
-										size="22"
-										color="green"
-									>mdi-nodejs</v-icon>
-								</v-list-item-icon>
-								<v-list-item-content>
-									<v-list-item-title>Delete /node_modules</v-list-item-title>
-								</v-list-item-content>
-							</v-list-item>
-							<v-list-item dense>
-								<v-list-item-icon class="mr-3">
-									<v-icon
-										size="22"
-										color="grey"
-									>mdi-playlist-remove</v-icon>
-								</v-list-item-icon>
-								<v-list-item-content>
-									<v-list-item-title>Ignore</v-list-item-title>
-								</v-list-item-content>
-							</v-list-item>
-						</v-list>
-					</v-menu>
-				</v-list-item-action>
-			</v-list-item>
-		</v-list>
-	</v-container>
+    <v-combobox
+      v-model="excludes"
+      dense
+      multiple
+      placeholder="C:"
+      class="mx-4 mt-4"
+      label="Excludes"
+    />
+
+    <div class="px-4 caption truncate" v-if="currentSearchDir">
+      <v-progress-circular indeterminate color="primary" size="12" width="1" />
+      {{ currentSearchDir }}
+    </div>
+    <v-toolbar flat dense class="mx-4" v-if="selectedNodeModules.length">
+      <v-btn small depressed color="error" @click="selectAll">
+        <v-icon left>mdi-check</v-icon>
+        Select All
+      </v-btn>
+      <v-btn small depressed color="error" @click="handleBulkDeleteNodeModules">
+        <v-icon left>mdi-delete</v-icon>
+        Delete selected
+      </v-btn>
+    </v-toolbar>
+    <v-list dense class="px-4" color="transparent">
+      <v-list-item
+        two-line
+        :key="dirIndex"
+        :class="[
+          dir.isSelected ? 'primary lighten-4' : 'white',
+          { 'mt-1': dirIndex },
+        ]"
+        v-for="(dir, dirIndex) in nodeModules"
+      >
+        <v-list-item-avatar
+          class="grey lighten-2"
+          @click="dir.isSelected = !dir.isSelected"
+        >
+          <v-checkbox
+            v-if="dir.isSelected"
+            readonly
+            hide-details
+            class="mt-0 pt-0"
+            style="width:24px"
+            v-model="checkboxIsChecked"
+          />
+          <v-icon v-else dark color="primary">mdi-folder</v-icon>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title>{{ dir.appName }}</v-list-item-title>
+          <v-list-item-subtitle>{{ dir.appPath }}</v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-menu transition="slide-x-transition" bottom right>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn icon v-bind="attrs" v-on="on">
+                <v-icon size="20" color="grey" class="text--darken-1"
+                  >mdi-dots-vertical</v-icon
+                >
+              </v-btn>
+            </template>
+            <v-list dense>
+              <!-- OPEN IN FILE EXPLORER -->
+              <v-list-item
+                dense
+                @click="handleOpenInExplorer(dir.nodeModulesPath)"
+              >
+                <v-list-item-icon class="mr-3">
+                  <v-icon size="22" color="orange">mdi-folder</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>Open in Explorer</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <!-- OPEN WITH VS CODE -->
+              <v-list-item dense @click="handleOpenInVSCode(dir.appPath)">
+                <v-list-item-icon class="mr-3">
+                  <v-icon size="22" color="blue"
+                    >mdi-microsoft-visual-studio-code</v-icon
+                  >
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>Open with Code</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <!-- ACTIONS -->
+              <v-divider></v-divider>
+              <v-list-item
+                dense
+                @click="handleDeleteNodeModules([dir.nodeModulesPath])"
+              >
+                <v-list-item-icon class="mr-3">
+                  <v-icon size="22" color="green">mdi-nodejs</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>Delete /node_modules</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item dense>
+                <v-list-item-icon class="mr-3">
+                  <v-icon size="22" color="grey">mdi-playlist-remove</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>Ignore</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-list-item-action>
+      </v-list-item>
+    </v-list>
+  </v-container>
 </template>
 
 <script>
-import _ from 'lodash'
+import _ from "lodash";
 
 const {
-	TEST,
-	EACH_DIR,
-	OPEN_IN_EXPLORER,
-	FIND_NODE_MODULES,
-	OPEN_WITH_VS_CODE,
-	DELETE_NODE_MODULES,
-	PROGRESS_NODE_MODULES,
-	CANCELLED_SEARCHING_NODE_MODULES,
-	PROGRESS_DELETE_NODE_MODULES,
-	GET_SUB_DIRS
-} = require('../consts')._COMMANDS
+  TEST,
+  EACH_DIR,
+  OPEN_IN_EXPLORER,
+  FIND_NODE_MODULES,
+  OPEN_WITH_VS_CODE,
+  DELETE_NODE_MODULES,
+  PROGRESS_NODE_MODULES,
+  CANCELLED_SEARCHING_NODE_MODULES,
+  PROGRESS_DELETE_NODE_MODULES,
+  GET_SUB_DIRS,
+} = require("../consts")._COMMANDS;
 
 export default {
-	name: 'Home',
-	data: () => ({
-		loading: false,
-		checkboxIsChecked: true,
-		currentSearchDir: null,
-		nodeModules: [],
-		currentDir: { path: 'W:\\' },
-		currentDirSubDirs: []
-	}),
-	computed: {
-		currentDirModel: {
-			get() {
-				return this.currentDir
-			},
-			set(v) {
-				if (!v) return (this.currentDir = null)
+  name: "Home",
+  data: () => ({
+    loading: false,
+    checkboxIsChecked: true,
+    currentSearchDir: null,
+    nodeModules: [],
+    currentDir: { path: "W:\\" },
+    currentDirSubDirs: [],
+    excludes: [".git", "golang"],
+    includes: [
+      "node_modules",
+      "node_modules-pn",
+      ".gradle",
+      ".idea",
+      ".next",
+      ".nuxt",
+      "vendor",
+      ".output",
+      "dist",
+      "site-packages",
+      ".yarn",
+      "cache",
+    ],
+  }),
+  computed: {
+    currentDirModel: {
+      get() {
+        return this.currentDir;
+      },
+      set(v) {
+        if (!v) return (this.currentDir = null);
 
-				if (!_.isString(v)) this.currentDir = v
-				else this.currentDir = { path: v }
+        if (!_.isString(v)) this.currentDir = v;
+        else this.currentDir = { path: v };
 
-				if (!this.currentDir.path.endsWith('\\')) {
-					this.currentDir.path += '\\'
-				}
+        if (!this.currentDir.path.endsWith("\\")) {
+          this.currentDir.path += "\\";
+        }
 
-				this.onGetSubDirs(this.currentDir)
-			}
-		},
-		selectedNodeModules() {
-			return this.nodeModules.filter(nm => {
-				return nm.isSelected
-			})
-		}
-	},
-	created() {
-		let local = localStorage.getItem('___aaa-bbb-aaa___')
-		if (local) this.nodeModules = JSON.parse(local)
+        this.onGetSubDirs(this.currentDir);
+      },
+    },
+    selectedNodeModules() {
+      return this.nodeModules.filter((nm) => {
+        return nm.isSelected;
+      });
+    },
+  },
+  created() {
+    let local = localStorage.getItem("___aaa-bbb-aaa___");
+    if (local) this.nodeModules = JSON.parse(local);
 
-		this.onGetSubDirs(this.currentDir)
+    this.onGetSubDirs(this.currentDir);
 
-		this.$ipc.on(PROGRESS_NODE_MODULES, dir => {
-			// let exist = this.nodeModules.some(
-			// 	nm => nm.nodeModulesPath === dir.nodeModulesPath
-			// )
-			// if (!exist) {
-			this.nodeModules.unshift(dir)
-			localStorage.setItem(
-				'___aaa-bbb-aaa___',
-				JSON.stringify(this.nodeModules)
-			)
-			// }
-		});
-		this.$ipc.on(EACH_DIR, dir => { this.currentSearchDir = dir });
-		this.$ipc.on(PROGRESS_DELETE_NODE_MODULES, dir => {
-			this.nodeModules = this.nodeModules.filter(nmDir => {
-				return nmDir.nodeModulesPath !== dir
-			})
-		});
-	},
-	methods: {
-		onGetSubDirs(dir) {
-			this.$ipc.emit(GET_SUB_DIRS, dir, subDirs => {
-				this.currentDirSubDirs = subDirs
-			})
-		},
-		sendTest() {
-			this.$ipc.emit(TEST, { message: 'Hello' }, res => {
-				console.log(res, JSON.stringify(res))
-			})
-		},
-		toggleSelectNodeModule(index) {
-			if (this.selectedNodeModulesIndexes.includes(index))
-				this.selectedNodeModulesIndexes =
-					this.selectedNodeModulesIndexes.filter(i => i !== index)
-			else this.selectedNodeModulesIndexes.push(index)
-		},
-		handleSearchNodeModules() {
-			this.loading = true
-			this.nodeModules = []
-			this.$ipc.emit(FIND_NODE_MODULES, this.currentDir, () => {
-				this.loading = false
-				this.currentSearchDir = null
-			});
-		},
-		handleCancelSearchNodeModules() {
-			this.$ipc.emit(CANCELLED_SEARCHING_NODE_MODULES, () => {
-				this.loading = false
-				this.currentSearchDir = null
-			});
-		},
-		handleOpenInExplorer(dir) {
-			this.$ipc.emit(OPEN_IN_EXPLORER, dir)
-		},
-		handleOpenInVSCode(dir) {
-			this.$ipc.emit(OPEN_WITH_VS_CODE, dir)
-		},
-		handleBulkDeleteNodeModules() {
-			this.handleDeleteNodeModules(
-				this.selectedNodeModules.map(nm => {
-					return nm.nodeModulesPath
-				})
-			)
-		},
-		handleDeleteNodeModules(dirs) {
-			this.$ipc.emit(DELETE_NODE_MODULES, dirs)
-		},
-	}
-}
+    this.$ipc.on(PROGRESS_NODE_MODULES, (dir) => {
+      // let exist = this.nodeModules.some(
+      // 	nm => nm.nodeModulesPath === dir.nodeModulesPath
+      // )
+      // if (!exist) {
+      this.nodeModules.unshift(dir);
+      localStorage.setItem(
+        "___aaa-bbb-aaa___",
+        JSON.stringify(this.nodeModules)
+      );
+      // }
+    });
+    this.$ipc.on(EACH_DIR, (dir) => {
+      this.currentSearchDir = dir;
+    });
+    this.$ipc.on(PROGRESS_DELETE_NODE_MODULES, (dir) => {
+      this.nodeModules = this.nodeModules.filter((nmDir) => {
+        return nmDir.nodeModulesPath !== dir;
+      });
+    });
+  },
+  methods: {
+    selectAll() {
+      this.nodeModules = this.nodeModules.map((v) => ({
+        ...v,
+        isSelected: true,
+      }));
+    },
+    onGetSubDirs(dir) {
+      this.$ipc.emit(GET_SUB_DIRS, dir, (subDirs) => {
+        this.currentDirSubDirs = subDirs;
+      });
+    },
+    sendTest() {
+      this.$ipc.emit(TEST, { message: "Hello" }, (res) => {
+        console.log(res, JSON.stringify(res));
+      });
+    },
+    toggleSelectNodeModule(index) {
+      if (this.selectedNodeModulesIndexes.includes(index))
+        this.selectedNodeModulesIndexes = this.selectedNodeModulesIndexes.filter(
+          (i) => i !== index
+        );
+      else this.selectedNodeModulesIndexes.push(index);
+    },
+    handleSearchNodeModules() {
+      this.loading = true;
+      this.nodeModules = [];
+      this.$ipc.emit(
+        FIND_NODE_MODULES,
+        {
+          dir: this.currentDir,
+          includes: this.includes,
+          excludes: this.excludes,
+        },
+        () => {
+          this.loading = false;
+          this.currentSearchDir = null;
+        }
+      );
+    },
+    handleCancelSearchNodeModules() {
+      this.$ipc.emit(CANCELLED_SEARCHING_NODE_MODULES, () => {
+        this.loading = false;
+        this.currentSearchDir = null;
+      });
+    },
+    handleOpenInExplorer(dir) {
+      this.$ipc.emit(OPEN_IN_EXPLORER, dir);
+    },
+    handleOpenInVSCode(dir) {
+      this.$ipc.emit(OPEN_WITH_VS_CODE, dir);
+    },
+    handleBulkDeleteNodeModules() {
+      this.handleDeleteNodeModules(
+        this.selectedNodeModules.map((nm) => {
+          return nm.nodeModulesPath;
+        })
+      );
+    },
+    handleDeleteNodeModules(dirs) {
+      this.$ipc.emit(DELETE_NODE_MODULES, dirs);
+    },
+  },
+};
 </script>
